d3a95eaf10c3e45ad91ea2bdd9ca61f8c3c3f483
John Hamelink <john@johnhamelink.com>
2016-09-27 03:43:26 +0100

    Improve error handling - prefer to be quiet

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,8 @@ fn main() {
         let mut ap = ArgumentParser::new();
         ap.set_description("BTC Ticker");
         ap.refer(&mut currency)
-            .add_option(&["-c", "--currency"], Store,
+            .add_option(&["-c", "--currency"],
+                        Store,
                         "3-letter character code for currency (such as USD)");
         ap.parse_args_or_exit();
     }
@@ -47,12 +48,17 @@ fn get_price(currency: String) -> (String) {
     let client = Client::new();
     let url = format!("https://api.coinbase.com/v2/prices/BTC-{}/spot", currency);
 
-    let mut res = client.get(&url)
-        .send()
-        .unwrap();
+    let res = client.get(&url).send().unwrap();
 
-    assert_eq!(res.status, hyper::Ok);
+    return match res.status {
+        hyper::Ok => retrieve_result(res),
+        _ => "".to_string(),
+    };
+}
+
+fn retrieve_result(parent_res: hyper::client::response::Response) -> (String) {
     let mut body = String::new();
+    let mut res = parent_res;
     res.read_to_string(&mut body).unwrap();
 
     match json::decode(&body) {
